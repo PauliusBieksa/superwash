220fc231dbb653a788f24df4d83a3a6d33e93bc7
THEMAGICTRICK\Arthur <arthursokolov753@gmail.com>
2025-01-25 14:44:37 +0000

    Added a SFX Manager, and 2 test sfx

---
+++ Superwash/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,89 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SoundManager : MonoBehaviour
+{
+    //Reference this script in any other script and call PlaySound() from that other script
+
+    private List<AudioSource> audioSources;
+
+    void Update()
+    {
+        for (int i = 0; i < audioSources.Count; i++)
+        {
+            if (!audioSources[i].isPlaying)
+            {
+                Destroy(audioSources[i]);
+                audioSources.RemoveAt(i);
+            }
+        } 
+    }
+
+    public void PlaySound(AudioClip clip, float vol, float pitch)
+    {
+        gameObject.AddComponent<AudioSource>();
+        audioSources = GetComponents<AudioSource>().ToList();
+        for (int i = 0; i < audioSources.Count; i++)
+        {
+            if (audioSources[i].clip != null)
+            {
+                continue;
+            }
+            else
+            {
+                audioSources[i].playOnAwake = false;
+                audioSources[i].clip = clip;
+                audioSources[i].volume = vol;
+                audioSources[i].pitch = pitch;
+                audioSources[i].Play();
+                break;
+            }
+        }
+
+    }
+
+    public void PlaySound(AudioClip clip, float vol)
+    {
+        gameObject.AddComponent<AudioSource>();
+        audioSources = GetComponents<AudioSource>().ToList();
+        for (int i = 0; i < audioSources.Count; i++)
+        {
+            if (audioSources[i].clip != null)
+            {
+                continue;
+            }
+            else
+            {
+                audioSources[i].playOnAwake = false;
+                audioSources[i].clip = clip;
+                audioSources[i].volume = vol;
+                audioSources[i].Play();
+                break;
+            }
+        }
+
+    }
+
+    public void PlaySound(AudioClip clip)
+    {
+        gameObject.AddComponent<AudioSource>();
+        audioSources = GetComponents<AudioSource>().ToList();
+        for (int i = 0; i < audioSources.Count; i++)
+        {
+            if (audioSources[i].clip != null)
+            {
+                continue;
+            }
+            else
+            {
+                audioSources[i].playOnAwake = false;
+                audioSources[i].clip = clip;
+                audioSources[i].Play();
+                break;
+            }
+        }
+
+    }
+}
