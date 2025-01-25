611eb59aa4d270cbff74f4a73b3918da7efca00e
THEMAGICTRICK\Arthur <arthursokolov753@gmail.com>
2025-01-25 13:39:18 +0000

    Implemented Timer, Added Main scene, Installed Tmpro

--- Superwash/Assets/Scripts/Timer.cs
+++ Superwash/Assets/Scripts/Timer.cs
@@ -1,16 +1,41 @@
+using System;
 using UnityEngine;
+using TMPro;
+using UnityEngine.Rendering.UI;
 
 public class Timer : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] float initialTime;
+    [SerializeField] TextMeshProUGUI textComponent;
+
+    float minutes;
+    float seconds;
+    float currentTime;
+
     void Start()
     {
-        
+        currentTime = initialTime;
+
+        if (textComponent == null)
+        {
+            textComponent = GameObject.FindAnyObjectByType<TextMeshProUGUI>().GetComponent<TextMeshProUGUI>();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        currentTime -= Time.deltaTime;
+
+        minutes = Mathf.FloorToInt(currentTime / 60F);
+        seconds = Mathf.FloorToInt(currentTime - minutes * 60);
+
+        string niceTime = string.Format("{0:0}:{1:00}", minutes, seconds);
+        textComponent.text = niceTime;
+    }
+
+    public void AddTime(float addTime)
+    {
+        currentTime += addTime;
     }
 }
